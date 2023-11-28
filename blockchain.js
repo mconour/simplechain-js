aa0be33ed744ef45813365cdd52cf9bf12a22622
mconour <michaelconour@gmail.com>
2023-11-28 12:01:28 -0800

    complete Hash Calculation function

--- blockchain.js
+++ blockchain.js
@@ -8,4 +8,8 @@ class Block {
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
     }
+
+    calculateHash() {
+        return SHA256(this.index + this.timestamp + JSON.stringify(this.data) + this.previousHash).toString();
+    }
 }
