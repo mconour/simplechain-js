53aa290277ba5006c1f6738c792297cb3787a4a1
mconour <michaelconour@gmail.com>
2023-12-05 13:00:08 -0800

    add usage example && isValid function

--- blockchain.js
+++ blockchain.js
@@ -1,39 +1,56 @@
 const SHA256 = require('crypto-js/sha256');
 
 class Block {
-    constructor(index, timestamp, data, previousHash = '') {
-        this.index = index;
-        this.timestamp = timestamp;
-        this.data = data;
-        this.previousHash = previousHash;
-        this.hash = this.calculateHash();
-    }
+  constructor(index, timestamp, data, previousHash = '') {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+  }
 
-    calculateHash() {
-        return SHA256(this.index + this.timestamp + JSON.stringify(this.data) + this.previousHash).toString();
-    }
+  calculateHash() {
+    return SHA256(this.index + this.timestamp + JSON.stringify(this.data) + this.previousHash).toString();
+  }
 }
 
 class Blockchain {
-    constructor() {
-        this.chain = [this.createGenesisBlock()];
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() {
+    return new Block(0, '01/01/2023', 'Genesis Block', '0');
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+
+  isValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash() || currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
     }
 
-    createGenesisBlock() {
-        return new Block(0, '01/01/2024', 'Genesis Block', '0');
-    }
-
-    getLatestBlock() {
-        return this.chain[this.chain.length - 1];
-      }
-    
-      addBlock(newBlock) {
-        newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
-        this.chain.push(newBlock);
-      }
+    return true;
+  }
 }
-
 
 // Usage example
 const myBlockchain = new Blockchain();
+myBlockchain.addBlock(new Block(1, '02/01/2023', { amount: 4 }));
+myBlockchain.addBlock(new Block(2, '03/01/2023', { amount: 8 }));
+
+console.log(JSON.stringify(myBlockchain, null, 2));
+console.log('Is blockchain valid? ' + myBlockchain.isValid());
